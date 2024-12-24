33a0dedc1961802dd4f4fc30944f29d2e3f29557
SAwandya <sachilaawandya@gmail.com>
2024-12-24 13:34:04 +0530

    create model for interview

---
+++ Backend/models/interview.js
@@ -0,0 +1,46 @@
+const Joi = require("joi");
+const { default: mongoose } = require("mongoose");
+
+const interviewSchema = new mongoose.Schema({
+    label: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        required: true,
+    },
+    time: {
+        type: String,
+        required: true,
+    },
+    schema: {
+        type: String,
+        required: true,
+    },
+    location: {
+        type: String,
+        required: true,
+    },
+    notes: {
+        type: String,
+    },
+});
+
+const Interview = mongoose.model("Interview", interviewSchema);
+
+function validateInterview(interview) {
+    const schema = Joi.object({
+        label: Joi.string().required(),
+        date: Joi.date().required(),
+        time: Joi.string().required(),
+        schema: Joi.string().required(),
+        location: Joi.string().required(),
+        notes: Joi.string(),
+    });
+
+    return schema.validate(interview);
+}
+
+module.exports.Interview = Interview;
+module.exports.validate = validateInterview;
